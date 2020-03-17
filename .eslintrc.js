614da1cb18ea396b2de606382b4005d575eb5ec0
mFlaifel <msf-2oo7@hotmail.com>
2020-03-17 16:19:20 +0200

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,54 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    'plugin:react/recommended',
+  "arrow-body-style": ["error", "as-needed"],
+  'extends': [
     'airbnb',
+    'prettier',
+    'prettier/react'
   ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+  // babel-eslint parser is used to support experimental features not supported in ESLint itself yet
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaVersion': 8,
+    'ecmaFeatures': {
+      'impliedStrict': true, //enable global strict mode (if ecmaVersion is 5 or greater)
+    }
   },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
+  root: true,
+  'env': {
+    'browser': true,
+    'node': true,
+    'jest': true
   },
-  plugins: [
+  'rules': {
+    //  allow .js extensions for JSX.
+    'react/jsx-filename-extension': [
+      1,
+      {
+        'extensions': [
+          '.js',
+          '.jsx'
+        ]
+      }
+    ],
+    'quotes': [
+      2,
+      'single',
+      {
+        'avoidEscape': true, // allows strings to use single-quotes or double-quotes so long as the string contains a quote that would have to be escaped otherwise
+        'allowTemplateLiterals': true // allows strings to use backticks
+      }
+    ],
+    // configure the prettier plugin
+    'prettier/prettier': [
+      'error',
+      {
+        'trailingComma': 'es5',
+        'singleQuote': true,
+      }
+    ]
+  },
+  'plugins': [
     'react',
-  ],
-  rules: {
-  },
+    'prettier'
+  ]
 };
