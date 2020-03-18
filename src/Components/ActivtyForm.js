8aa55ae053b446b02f6875f4be22d80a8433f10c
mFlaifel <msf-2oo7@hotmail.com>
2020-03-18 16:32:58 +0200

    show data relates #6

--- src/Components/ActivtyForm.js
+++ src/Components/ActivtyForm.js
@@ -1,20 +1,81 @@
 import React from "react";
-import data from '../resources/data';
+import data from "../resources/data";
+class ActivityForm extends React.Component {
+  state = {
+    activity: "Aerobics",
+    day: "Saturday",
+    userData: {
+      Saturday: ["Walking"],
+      Sunday: [],
+      Monday: [],
+      Tuesday: [],
+      Wedensday: [],
+      Thursday: [],
+      Friday: []
+    }
+  };
 
- const ActivityForm =()=>{
-  const dataList =data;
-  console.log(dataList.activity)
-  return (
-   
-      <label >Activity Type:
-      <select id="activity">
-     { dataList.activity.map(e=>{
-       return  <option value={e} key={e}>{e}</option>;
-      })};
-      </select>
-      </label>
-   
-  )
+  tempUserData = JSON.parse(JSON.stringify(this.state.userData));
 
+  updateActivity = e => {
+    this.setState({ activity: e.target.value });
+  };
+
+  updateValues = () => {
+    this.tempUserData[this.state.day].push(this.state.activity);
+
+    this.setState({ userData: this.tempUserData });
+  };
+
+  updateDay = e => {
+    this.setState({ day: e.target.value });
+  };
+
+  render() {
+    return (
+      <main>
+        <div className="activity">
+          <label>
+            Day:
+            <select id="activity" onChange={this.updateDay}>
+              {data.days.map(e => {
+                return (
+                  <option value={e} key={e}>
+                    {e}
+                  </option>
+                );
+              })}
+              ;
+            </select>
+          </label>
+          <label>
+            Activity Type:
+            <select id="activity" onChange={this.updateActivity}>
+              {data.activity.map(e => {
+                return (
+                  <option value={e} key={e}>
+                    {e}
+                  </option>
+                );
+              })}
+              ;
+            </select>
+          </label>
+          <button onClick={this.updateValues}>add to schedule</button>
+        </div>
+
+        <div className="container">
+          {data.days.map(e => (
+            <ul className={e}>
+              {e}
+              {this.state.userData[e].map(e => (
+                <li key={e}>{e}</li>
+              ))}
+            </ul>
+          ))}
+        </div>
+      </main>
+    );
+  }
 }
 export default ActivityForm;
--- src/resources/data.js
+++ src/resources/data.js
@@ -1,7 +1,33 @@
-const data ={
-  days:['Saturday','Sunday','Monday','Tuesday','Wedensday','Thursday','Friday'],
-  activity:['Aeobics','Basketball','Biking','Dancing','Diving','Football','Golf','Hockey','Jogging','Running','Sand runngin','Swimming','Table tennis','Tennis','Treadmill','Walking','Yoga','Zumbie Zumba'],
-  userData:{'Saturday':[],'Sunday':[],'Monday':[],'Tuesday':[],'Wedensday':[],'Thursday':[],'Friday':[]}
-}
+let data = {
+  days: [
+    "Saturday",
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wedensday",
+    "Thursday",
+    "Friday"
+  ],
+  activity: [
+    "Aerobics",
+    "Basketball",
+    "Biking",
+    "Dancing",
+    "Diving",
+    "Football",
+    "Golf",
+    "Hockey",
+    "Jogging",
+    "Running",
+    "Sand runngin",
+    "Swimming",
+    "Table tennis",
+    "Tennis",
+    "Treadmill",
+    "Walking",
+    "Yoga",
+    "Zumbie Zumba"
+  ]
+};
 
 export default data;
